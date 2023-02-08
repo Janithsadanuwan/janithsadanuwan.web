2acfaf75af014f97c2e37ea89312f94af045dae4
Janith sadanuwan/ <95123183+Janithsadanuwan@users.noreply.github.com>
2023-02-08 09:35:18 +0530

    Add files via upload

---
+++ QueenNilu/script.js
@@ -0,0 +1,18 @@
+let menu = document.querySelector('#menu-bars');
+let header = document.querySelector('header');
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    header.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    header.classList.remove('active');
+}
+
+function websiteVisits(response) {
+    document.querySelector("#visits").textContent = response.value;
+}
+
+
